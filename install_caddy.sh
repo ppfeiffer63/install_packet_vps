5ab3c0145fdf4f06ca4a012589aa1a2a7b512846
Pfeiffer <PfeifferP@dreebitnet.local>
2024-08-26 06:19:15 +0200

    import installfile

---
+++ install_caddy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Init
+
+#....
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# ...
+
+apt update
+apt-get install -y curl debian-keyring debian-archive-keyring apt-transport-https
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | apt-key add -
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+
+apt-get update
+apt-get install caddy -y
+
+cat Caddyfile >> /etc/caddy/Caddyfile
