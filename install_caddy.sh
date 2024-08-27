97bc0f9f43b1c8a8e5960b650f70eb37b05efbf0
ppfeiffer <admin@pfeiffer-privat.de>
2024-08-27 13:16:28 +0200

    install_caddy.sh aktualisiert

--- install_caddy.sh
+++ install_caddy.sh
@@ -15,7 +15,7 @@ apt-get install -y curl debian-keyring debian-archive-keyring apt-transport-http
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
-apt install caddy
+apt install caddy cockpit
 
 
 
