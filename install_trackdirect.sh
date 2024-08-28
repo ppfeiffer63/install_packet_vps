7d5d984f5496e638ed39a281f33e36849fb78788
ppfeiffer <admin@pfeiffer-privat.de>
2024-08-28 05:57:39 +0200

    install_trackdirect.sh aktualisiert

--- install_trackdirect.sh
+++ install_trackdirect.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+# Init
+
+#....
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# ...
+
 git clone https://gitea.home.pfeiffer-privat.de/ppfeiffer/trackdirect.git
 cd trackdirect/
 docker compose up -d
 cat files/caddy_trackdirect >> /etc/caddy/Caddyfile
+systemctl restart caddy
